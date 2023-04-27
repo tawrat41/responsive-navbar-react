a7fb1c25afe0ac3f723beb3ce6c403e866264019
tawrat41 <tawratur.rashid@gmail.com>
2023-04-27 23:41:45 +0600

    rechart done

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './components/Navbar/Navbar'
 import PriceList from './components/PriceList/PriceList'
+import Rechart from './components/Rechart/Rechart'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -14,6 +15,7 @@ function App() {
         <Navbar></Navbar>
         <h1 className='text-center text-7xl'>Hello from Tailwind</h1>
         <PriceList></PriceList>
+        <Rechart></Rechart>
       </div>
     </>
   )
