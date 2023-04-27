c4be282f4498ae95559afd75ffe2def5f7f1b64c
tawrat41 <tawratur.rashid@gmail.com>
2023-04-27 20:18:08 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Navbar from './components/Navbar/Navbar'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <Navbar></Navbar>
+        <h1 className='text-7xl'>Hello from Tailwind</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Link/Link.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Link = ({route}) => {
+  return (
+    <li className='mr-4 hover:bg-purple-600'>
+        <a href={route.path}>{route.name}</a>
+        
+    </li>
+  )
+}
+
+export default Link;
